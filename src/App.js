45290045e41b0ca1772f1133a9d4465d32e72857
Prit <prit.italiya@yahoo.com>
2020-06-07 15:07:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {connect} from 'react-redux';
+import Spinner from "./shared/components/Spinner";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    let routes = (
+        <Switch>
+          <Route exact path="/" component={Welcome}/>
+        </Switch>
+    );
+
+    return (
+        <div className='bootstap-layout'>
+          <div className='container'>
+            <BrowserRouter>
+              <div className='root-component'>
+                <Spinner show={this.props.isLoading}/>
+                {routes}
+              </div>
+            </BrowserRouter>
+          </div>
+        </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  isLoading: state.config.isLoading,
+});
+
+
+export default connect(mapStateToProps, null)(App);
